696328425e48b59d2e69943804c962cb67ee1ef0
ekxhta <ekshta.mishra29@gmail.com>
2024-10-05 09:38:10 +0530

    2nd October 2024 Operators and conditionals

---
+++ Javascript/operators.js
@@ -0,0 +1,29 @@
+// Operators are special symbols that help in executing some operations
+// they can be : arithmetic , unary, binary
+
+// arithmetic operator
+console.log("Arithmetic operators");
+   let a = 10;
+   let b = 5;
+   console.log(a+b)
+   console.log(a-b);
+   console.log(a*b);
+   console.log(a/b);
+   console.log(a%b);
+   console.log(a**b);
+
+//    unary operators
+//  ++ - two types : pre increment ++a; and post increment ie a++
+console.log("Unary operators");
+console.log(++a); //increment then print
+console.log(a++); //print then increment
+
+// similarly for predecrement and post decrement 
+console.log(--b);
+console.log(b--);
+
+// Assignment operator
+let x=5; //= here is assignment operator
+x=x+5;
+x+=5; //shorthand notation for above
+
